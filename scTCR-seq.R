637bbc0310c9ac9a4c7047966f4b00faa2ccb26d
sugi_ACConcoimuno <95730123+sugiACConcoimmunology@users.noreply.github.com>
2023-10-24 16:39:13 +0900

    Update scTCR-seq.R

--- scTCR-seq.R
+++ scTCR-seq.R
@@ -2,7 +2,7 @@ suppressMessages(library(scRepertoire))
 library(Seurat) 
 
 # load data
-MPE.combined_label2 <- readRDS("G:/SingleCell/singlecellfile/MPE-CD4_MPE.combined_label2.rds")
+MPE.combined_label2 <- readRDS("G:/SingleCell/singlecellfile/MPE_MPE.combined_label2.rds")
 SKOM002 <- read.csv("/SingleCell/KOM002/KOM002_vdj2/outs/filtered_contig_annotations.csv")
 SLK208 <- read.csv("/SingleCell/LK208/LK208_vdj_outs/filtered_contig_annotations.csv")
 SLK006 <- read.csv("/SingleCell/LK006/outs/filtered_contig_annotations.csv")
